2617fbe1df79afe5c725197561c4eeb9af9cae24
David Bergqvist <David.bergqvist@gmail.com>
2024-06-14 07:28:14 +0000

    added componets functions

---
+++ my-app/src/components/FunctionalGreetings.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "../App";
+
+function FunctionalGreeting() {
+    return <h1>Hello from React!</h1>
+}
+
+export default FunctionalGreeting;
---
+++ my-app/src/components/FunctionalGreetingsWithProps.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "../App";
+
+function FunctionalGreetingWithProps() {
+    return <h1>Hello!</h1>
+}
+
+export default FunctionalGreetingWithProps;
